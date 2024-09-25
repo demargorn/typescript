4927227f44b7f8cfa0af3682af3665251fc329ee
Михаил Дорошин <demargorn@gmail.com>
2024-09-25 17:20:14 +0300

    bug fixed

--- src/__tests__/Cart.ts
+++ src/__tests__/Cart.ts
@@ -3,7 +3,6 @@ import { Movie } from '../Movie';
 
 const cart = new Cart();
 const matrix = new Movie('The Matrix', 2000, 'USA', 'sjhljsc', 'fantasy', '03:12');
-const hitman = new Movie('Hitman', 2000, 'USA', 'sjhljsc', 'drama', '03:20');
 
 test('test Car add method with %o items', () => {
    cart.add(matrix);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,2 @@
-import { IMovie } from './interfaces/IMovie';
-import { Movie } from './Movie';
-import { Cart } from './Cart';
-
-const cart = new Cart()
-const avengers = new Movie('Avengers', 2015, 'USA', 'Avengers Aussome', 'fantasy', '02:12');
-
-cart.add(avengers)
-
-console.log(avengers.showMovie);
-console.log(avengers.showMovie);
-console.log(cart);
+import './Movie';
+import './Cart';
